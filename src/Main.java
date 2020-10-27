061c430a1a3f74d8dd5b5423645e115abc6a8b07
Venzel <venzelseo@gmail.com>
2020-10-26 22:42:01 -0300

    apenas fiz um comentario na main

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Payment pay = new Payment();
 
+        // Comentario teste
         int valueFinal = pay.PicPay(10);
 
         System.out.println(valueFinal);
